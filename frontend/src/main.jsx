3a8b95be06eb9096f39a6dd424819ea39372d8af
christopher639 <bundichristopher639@gmail.com>
2025-05-19 19:06:07 +0300

    deploying

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // <-- Add this import
-import './index.css'
+import {BrowserRouter} from "react-router-dom"
 import App from './App.jsx'
-
+import './index.css'
+import { HashRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>  {/* <-- Wrap your App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
+
+
+
